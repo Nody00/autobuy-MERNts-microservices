b8f5547422192401691bd481224af5333109e46c
nody00 <dinokrcicprof@gmail.com>
2024-10-13 11:19:58 +0200

    added more test for the sign up route

--- auth/src/routes/__test__/signUp.test.ts
+++ auth/src/routes/__test__/signUp.test.ts
@@ -14,7 +14,7 @@ jest.mock("../../helpers/hashPassword.ts", () => ({
 
 describe("User Signup", () => {
   // Mock the User model methods
-  it("returns a 201 on successful signup", async () => {
+  it("returns a 201 on successful signup and sends appropirate RABBITMQ message", async () => {
     const newUser = {
       email: "testuser3@example.com",
       password: "TestPassword123",
@@ -27,7 +27,7 @@ describe("User Signup", () => {
       .send(newUser);
 
     expect(response.status).toBe(201);
-    expect(response.body.message).toEqual("User created");
+    expect(response.body.message).toBeDefined();
     expect(rabbit.sendMessage).toHaveBeenCalledWith(
       "auth-events",
       "users.new",
@@ -36,4 +36,39 @@ describe("User Signup", () => {
   });
 
   //   write more tests, finish signup and write tests for that
+  it("returns status 400 and an array of validation errors if the payload is missing information", async () => {
+    const newUser = {
+      email: "testuser3@example.com",
+      password: "1", // invalid
+      firstName: "John",
+      lastName: "Doe",
+      phoneNumber: "0", // invalid
+    };
+
+    const response = await request(app)
+      .post("/auth/users/sign-up")
+      .send(newUser);
+
+    expect(response.status).toBe(400);
+    expect(response?.body.errors.length).toEqual(2);
+  });
+
+  it("returns status 400 if the user already exists", async () => {
+    const newUser = {
+      email: "testuser3@example.com",
+      password: "TestPassword123",
+      firstName: "John",
+      lastName: "Doe",
+      phoneNumber: "0952230241",
+    };
+
+    await request(app).post("/auth/users/sign-up").send(newUser);
+
+    const response = await request(app)
+      .post("/auth/users/sign-up")
+      .send(newUser);
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBeDefined();
+  });
 });
