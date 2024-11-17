47d559a892667db69e67be62f43efc1d73290358
nody00 <dinokrcicprof@gmail.com>
2024-11-17 14:21:05 +0100

    bootstraped the notification service

--- notifications/src/app.ts
+++ notifications/src/app.ts
@@ -2,19 +2,23 @@ import express from "express";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 
+const BASE_ROUTE = "/notifications";
 const app = express();
 
 app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
-    signed: false,
-    secure: false,
+    name: "session",
+    keys: [process.env.COOKIE_SESSION_KEY!], // Use a secure, randomly generated key
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+    secure: false, // Secure cookies in production
+    httpOnly: true, // Prevents client-side JS from accessing the cookies
   })
 );
 
 app.all("*", async (req, res) => {
-  throw new Error("Route not found!");
+  res.status(404).send({ message: "Route not found" });
 });
 
 export { app };
--- notifications/src/index.ts
+++ notifications/src/index.ts
@@ -1,13 +1,30 @@
 import { app } from "./app";
+import { rabbit } from "./service/RabbitMQService";
+import mongoose from "mongoose";
 
 const start = async () => {
-  console.log("Notifications service booting up...");
+  console.log("notification service booting up...");
   try {
     // connect to mongoose and stuff
   } catch (error) {}
 
-  app.listen(3000, () => {
-    console.log("Notifications listening on port 3000...");
+  app.listen(4005, async () => {
+    if (!process.env.COOKIE_SESSION_KEY) {
+      console.error("COOKIE SESSION KEY MISSING");
+      return;
+    }
+    if (!process.env.JWT_SECRET!) {
+      console.error("JWT SECRET MISSING");
+      return;
+    }
+
+    await mongoose.connect("mongodb://notification-mongo-srv:27017");
+
+    await rabbit.connect("amqp://admin:password@rabbitmq-srv:5672");
+    // await rabbit.initializePublisher();
+    // await rabbit.initilizeNewListingConsumer();
+
+    console.log("notification listening on port 4005...");
   });
 };
 
