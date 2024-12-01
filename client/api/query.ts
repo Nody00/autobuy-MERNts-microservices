9e3279b455810836f8a78bb2d25698abba856c00
nody00 <dinokrcicprof@gmail.com>
2024-12-01 12:57:17 +0100

    added users api object

---
+++ client/api/query.ts
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+interface GetListingParams {
+  manufacturer?: string;
+  model?: string;
+  yearFrom?: number;
+  yearTo?: number;
+  priceMin?: number;
+  priceMax?: number;
+  mileageMin?: number;
+  mileageMax?: number;
+  category?: number;
+  status?: "available" | "sold";
+  isFeatured?: boolean;
+  negotiable?: boolean;
+  tags?: string;
+  sortBy?: "price" | "yearOfProduction" | "mileage" | "createdAt" | "views";
+  sortOrder?: "asc" | "desc";
+  page?: number;
+  limit?: number;
+}
+
+interface GetUsersParams {
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  emailVerified?: boolean;
+  phoneVerified?: boolean;
+  isAdmin?: boolean;
+  isCustomer?: boolean;
+  sortBy?: "email" | "firstName" | "lastName" | "createdAt";
+  sortOrder?: "asc" | "desc";
+  page?: number;
+  limit?: number;
+}
+
+export const queryAPI = Object.freeze({
+  getListings: async (params: GetListingParams) => {
+    const { data } = await axios.get("/query/listing", {
+      params: params,
+    });
+    return data;
+  },
+  getOneListing: async (id: string) => {
+    const { data } = await axios.get(`/query/listing/${id}`);
+    return data;
+  },
+  getUsers: async (params: GetUsersParams) => {
+    const { data } = await axios.get(`/query/users`, {
+      params: params,
+    });
+    return data;
+  },
+  getOneUser: async (id: string) => {
+    const { data } = await axios.get(`/query/users/${id}`);
+    return data;
+  },
+});
