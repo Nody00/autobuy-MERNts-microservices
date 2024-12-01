a2e3885b79decffb7c2308c133dc67a781a7b46f
nody00 <dinokrcicprof@gmail.com>
2024-12-01 12:19:49 +0100

    added auth routes to the authapi object

--- client/api/auth.ts
+++ client/api/auth.ts
@@ -14,6 +14,14 @@ interface signInPayload {
   password: string;
 }
 
+interface updateUserPayload {
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  recoveryPhoneNumber?: string;
+}
+
 export const authAPI = Object.freeze({
   signUp: async (payload: signUpPayload) => {
     const { data } = await axios.post("/auth/users/sign-up", payload);
@@ -22,5 +30,20 @@ export const authAPI = Object.freeze({
   signIn: async (payload: signInPayload) => {
     const { data } = await axios.post("/auth/users/sign-in", payload);
     return data;
+  },
+  signOut: async () => {
+    const { data } = await axios.post("/auth/sign-out");
+    return data;
+  },
+  refreshToken: async () => {
+    const { data } = await axios.post("/auth/refresh-token");
+    return data;
+  },
+  deleteUser: async (id: string) => {
+    const { data } = await axios.delete(`/auth/users/delete-user/${id}`);
+    return data;
+  },
+  updateUser: async (id: string, payload: updateUserPayload) => {
+    const { data } = await axios.patch(`/auth/users/${id}`, payload);
   },
 });
