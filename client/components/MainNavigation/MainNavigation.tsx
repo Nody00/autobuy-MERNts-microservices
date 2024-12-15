9fbddbaa77ce43625da141b6410ed0c029145e1d
nody00 <dinokrcicprof@gmail.com>
2024-12-15 14:19:43 +0100

    added icons and link buttons to the nav component

--- client/components/MainNavigation/MainNavigation.tsx
+++ client/components/MainNavigation/MainNavigation.tsx
@@ -1,13 +1,49 @@
+import { CarSideIcon } from "@/icons/CarSideIcon";
 import styles from "./MainNavigation.module.css";
-
+import { SettingsIcon } from "@/icons/SettingsIcon";
+import Link from "next/link";
+import { HeartIcon } from "@/icons/HeartIcon";
+import { PlusCircleIcon } from "@/icons/PlusCircleIcon";
+import { UserIcon } from "@/icons/UserIcon";
+import { UsersIcon } from "@/icons/UsersIcon";
 export const MainNavigation = () => {
   return (
     <div className={styles.container}>
-      <p>H</p>
-      <p>H</p>
-      <p>H</p>
-      <p>H</p>
-      <p>H</p>
+      <Link
+        href={"/new-listing"}
+        className={styles.linkButton}
+        title="New Listing"
+      >
+        <PlusCircleIcon size="24px" />
+      </Link>
+
+      <Link href={"/listings"} className={styles.linkButton} title="Listings">
+        <CarSideIcon size="24px" />
+      </Link>
+
+      <Link
+        href={"/saved"}
+        className={styles.linkButton}
+        title="Saved Listings"
+      >
+        <HeartIcon size="24px" />
+      </Link>
+
+      <Link
+        href={"/my-profile"}
+        className={styles.linkButton}
+        title="My Profile"
+      >
+        <UserIcon size="24px" />
+      </Link>
+
+      <Link href={"/users"} className={styles.linkButton} title="Manage Users">
+        <UsersIcon size="24px" />
+      </Link>
+
+      <Link href={"/settings"} className={styles.linkButton} title="Settings">
+        <SettingsIcon size="24px" />
+      </Link>
     </div>
   );
 };
