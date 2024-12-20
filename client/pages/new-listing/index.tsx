bd7d5cf82214aa4917750ba4aed7c68884caf40d
nody00 <dinokrcicprof@gmail.com>
2024-12-20 10:37:21 +0100

    work begins on new listing form

--- client/pages/new-listing/index.tsx
+++ client/pages/new-listing/index.tsx
@@ -1,9 +1,15 @@
 import styles from "./newListing.module.css";
 import Layout from "@/components/Layout/Layout";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 export default function NewListing() {
   return (
     <Layout>
-      <div className={styles.container}>new listings</div>
+      <div className={styles.container}>
+        <p className={styles.title}>Add a new listing</p>
+
+        {/* <Formik initialValues={{}}></Formik> */}
+      </div>
     </Layout>
   );
 }
