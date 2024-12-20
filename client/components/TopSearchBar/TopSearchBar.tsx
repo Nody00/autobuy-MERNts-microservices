9a6e7980063aec0d8a1a5b8e2148859dbfe4c1bf
nody00 <dinokrcicprof@gmail.com>
2024-12-20 10:14:00 +0100

    search bar work

--- client/components/TopSearchBar/TopSearchBar.tsx
+++ client/components/TopSearchBar/TopSearchBar.tsx
@@ -1,5 +1,23 @@
+import { UserIcon } from "@/icons/UserIcon";
 import styles from "./TopSearchBar.module.css";
+import { useAuthStore } from "@/stores/authStore";
+import SearchIcon from "@/icons/SearchIcon";
 
 export const TopSearchBar = () => {
-  return <div className={styles.container}>seach</div>;
+  const user = useAuthStore((state) => state.user);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.searchInput}>
+        <SearchIcon />
+        <input type="text" />
+      </div>
+
+      {/*  */}
+      <div className={styles.iconAndMailBox}>
+        <UserIcon size="16px" />
+        <p>{user?.email || ""}</p>
+      </div>
+    </div>
+  );
 };
